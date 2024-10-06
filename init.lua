3729e8a9b37df6e005fe2adc48d206a7de8d5f86
Aditya Tiwari <adityaatiwariii@gmail.com>
2024-10-06 14:33:51 +0530

    feat: add basic configurations to init lua file

--- init.lua
+++ init.lua
@@ -1,2 +1,31 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = { "lua", "javascript", "ruby" },
+  sync_install = false,
+  auto_install = true,
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
+require("catppuccin").setup()
+vim.cmd.colorscheme("catppuccin")
